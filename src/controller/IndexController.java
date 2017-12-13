965a90f8a83702152264e30f136eb57735c0bafa
cluo1 <cluo199@163.com>
2017-12-13 15:19:10 +0800

    r

--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -3,11 +3,13 @@ package controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller{
-
+@Controller
+public class IndexController{
+	@RequestMapping("/index")
 	public ModelAndView handleRequest(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
 		//ModelAndView mv = new ModelAndView("index.jsp");
